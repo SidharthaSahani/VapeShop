10ffaf454ed44003c2cf8f0cb2957398388996cf
Sidhartha_sahani <sidharthasahanii@gmail.com>
2025-11-28 09:16:32 +0545

    search bar fix

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
-import e from 'cors';
+// import e from 'cors';
 import { assets } from '../assets/assets';
 import { useLocation } from 'react-router-dom';
 
